b2752c49d5112e2e41cca2b1833ef90fb8bc869a
xiamem4 <maximecroissant5@gmail.com>
2026-02-12 15:09:01 +0100

    20260212_MC_update_cart_price_initialisation-reload-page

---
+++ serverRun.sh
@@ -0,0 +1,5 @@
+php bin/console doctrine:database:create --if-not-exists 2>NUL
+php bin/console doctrine:schema:drop --force
+php bin/console doctrine:schema:create
+php bin/console doctrine:fixtures:load --append
+php -S 0.0.0.0:8000 -t public
